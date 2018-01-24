0894044926d8ad75981cfaf9fb7a2e5e9945f83b
Keita Shimizu <Keita@MacBook-Pro.local>
2018-01-24 23:31:23 +0900

    add perceptron

--- 1_Python入門.py
+++ 1_Python入門.py
@@ -26,3 +26,53 @@ class cls:
 
 cls_test = cls('hello!')
 cls_test.display()
+
+
+import numpy as np
+
+ndarry1 = np.array([1,2,3])
+
+print(ndarry1)
+print(type(ndarry1))
+
+ndarry2 = np.array([2,4,6])
+
+print(ndarry1 - ndarry2)
+print(ndarry1 / 2)
+
+ndarry3 = np.array([[1, 2], [3, 4]])
+ndarry4 = np.array([[2, 4], [6, 8]])
+
+print(ndarry3 + ndarry4)
+print(ndarry3[1][0])
+
+print(ndarry3 > 2)
+print(ndarry3[ndarry3 > 2])
+
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 6, 0.1)
+y = np.sin(x)
+
+# plt.plot(x, y)
+# plt.show()
+
+
+#%%
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.image import imread
+import numpy as np
+
+x = np.linspace(0, 20, 100)
+y1 = np.sin(x)
+y2 = np.cos(x)
+plt.plot(x, y1, label='sin')
+plt.plot(x, y2, linestyle='--', label='cos')
+plt.xlabel('x-label') # x軸のラベル
+plt.ylabel('y-label') # y軸のラベル
+plt.title('sin & cos graph')
+img = imread('bird.png')
+plt.imshow(img)
+plt.legend()
+plt.show() 
---
+++ 2_パーセプトロン.py
@@ -0,0 +1,42 @@
+# x:input
+# w:weight
+# y:output
+# b:bias
+
+import numpy as np
+
+class perceptron:
+    def __init__(self, w, b):
+        self.w, self.b = w, b
+    def fire(self, x):
+        x = np.sum(x * self.w) + self.b # NumPyのブロードキャスト機能を使用
+        if(x > 0):
+            return 1
+        else:
+            return 0 
+
+f_f = np.array([0, 0])
+f_t = np.array([0, 1])
+t_f = np.array([1, 0])
+t_t = np.array([1, 1])
+
+print('-- AND GATE --')
+AND_gate = perceptron(np.array([0.5, 0.5]), -0.6)
+print(AND_gate.fire(f_f)) # -> 0
+print(AND_gate.fire(f_t)) # -> 0
+print(AND_gate.fire(t_f)) # -> 0
+print(AND_gate.fire(t_t)) # -> 1
+
+print('-- NAND GATE --')
+NAND_gate = perceptron(np.array([-0.5, -0.5]), 0.6)
+print(NAND_gate.fire(f_f)) # -> 1
+print(NAND_gate.fire(f_t)) # -> 1
+print(NAND_gate.fire(t_f)) # -> 1
+print(NAND_gate.fire(t_t)) # -> 0
+
+print('-- OR GATE --')
+OR_gate = perceptron(np.array([0.5, 0.5]), -0.4)
+print(OR_gate.fire(f_f)) # -> 0
+print(OR_gate.fire(f_t)) # -> 1
+print(OR_gate.fire(t_f)) # -> 1
+print(OR_gate.fire(t_t)) # -> 1
